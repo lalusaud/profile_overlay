8cde99df8e65144d5e0a6cbb472175803d29ac05
Lal B. Saud <lalusaud@gmail.com>
2015-10-02 18:48:15 -0400

    Remove x-frame options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :allow_iframe_requests
+  after_filter :allow_iframe_requests
 
   def allow_iframe_requests
-    response.headers.delete('X-Frame-Options')
+    response.headers.delete 'X-Frame-Options'
   end
 end
