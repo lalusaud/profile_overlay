2d8122cc3d0cbd569e3a2196a6df1aaa43cde0c7
Lal B. Saud <lalusaud@gmail.com>
2015-10-02 18:18:12 -0400

    Add default post route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'overlay#index'
+  post '/' => 'overlay#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
