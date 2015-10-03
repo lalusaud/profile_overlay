b944120e5508048cbc5a9ea454f4f2046d262da8
Lal B. Saud <lalusaud@gmail.com>
2015-10-03 14:38:07 -0400

    Remove koala code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
 
-  def self.koala(auth)
-    access_token = auth['token']
-    facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=name,picture")
-  end
-
   def self.from_omniauth(auth_hash)
     user = User.new
     user.id = auth_hash['uid']
--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module ProfileOverlay
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    Koala.config.api_version = 'v2.0'
   end
 end
