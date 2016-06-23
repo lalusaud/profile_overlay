862c4a7c8bf21ea9fb0ca67d26b7c272b07672f8
Lalu Saud <lalusaud@gmail.com>
2016-06-22 23:19:43 -0400

    Change title and fixed failing tests

--- spec/features/static_pages_view_spec.rb
+++ spec/features/static_pages_view_spec.rb
@@ -10,9 +10,8 @@ RSpec.feature "Static pages view", type: :feature do
     end
 
     it 'has right title and guest login message' do
-      expect(page).to have_title('NP Profile Image Overlay')
-      expect(page).to have_selector('h1', 'NP Profile Image Overlay')
-      expect(page).to have_content('Hello Guest')
+      expect(page).to have_title('#BestFansInTheWorld!')
+      expect(page).to have_selector('h1', 'Huawei, Ireland')
     end
   end
 
